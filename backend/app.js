fb8af8e49534780cc205ecac793e66be4c404105
unknown <dennaligrissom7@gmail.com>
2020-12-19 10:49:19 -0500

    get

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,20 @@
 //Express Hello World Ex: https://expressjs.com/en/starter/hello-world.html
 const express = require('express'); // importing express
+var request = require('request');
 const app = express();
 const port = 5000; // change from 3000 5000 as 3000 is where react is ran
 // start with node app.js
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// });
+
+app.get("/getPicture",(req, res)=> 
+request('https://www.flickr.com/search/?q=', function (error, response, body){
+  if(!error && response.statusCode == 200){
+    console.log(body);
+  }
+}));
 
 
 app.listen(port, () => {
