590b4bc074b1920aa03e2bf0964ea07a2b35d14a
unknown <dennaligrissom7@gmail.com>
2020-12-21 21:39:36 -0500

    adds env elements

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 //Express Hello World Ex: https://expressjs.com/en/starter/hello-world.html
 // must rerun to refresh changes 
 const express = require('express'); // importing express
@@ -11,12 +13,11 @@ const port = 5000; // change from 3000 5000 as 3000 is where react is ran
 // });
 
 //K
-//77df9cfc809ffd3dc     (1)
-// ------
-//742f70e44786ea0       (2)
-var myWeatherKey= String(process.env.WEATHERKEY); // dot env currently not working  ---> youtube vid : https://www.youtube.com/watch?v=HRBNeERE5PU 
+// console.log(process.env);
+const myKey= process.env.WEATHERSTACK_KEY // dot env currently not working  ---> youtube vid : https://www.youtube.com/watch?v=HRBNeERE5PU 
+//https://www.youtube.com/watch?v=17UVejOw3zA
 app.get("/getWeathertoronto", (req, res)=>{
-  request("http://api.weatherstack.com/current?access_key="+"KEY PLACEHOLDER"+"&query='New York'", 
+  request("http://api.weatherstack.com/current?access_key="+String(myKey)+"&query='New York'", 
   function(error, response, body){
     if(!error && response.statusCode == 200){
       
