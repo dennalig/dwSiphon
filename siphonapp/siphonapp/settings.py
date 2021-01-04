588fe5d0aca93fd2e00693a70c837df3a5c08544
dennalig <dennaligrissom7@gmail.com>
2021-01-04 17:45:46 -0500

    hid django secret key

--- siphonapp/siphonapp/settings.py
+++ siphonapp/siphonapp/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-fgjfh4oe1h8@vyni0c!k4h&y@qe08c_7jeg0kmi_i3y^4!(l*'
+
+SECRET_KEY = str(os.environ.get('DWSIPHION_DJANGO_SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
