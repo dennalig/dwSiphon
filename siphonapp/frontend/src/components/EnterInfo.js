b95072bd20ce963a18daf819f79ab6bca0a1fddc
dennalig <dennaligrissom7@gmail.com>
2021-01-08 21:59:18 -0500

    fix

--- siphonapp/frontend/src/components/EnterInfo.js
+++ siphonapp/frontend/src/components/EnterInfo.js
@@ -6,9 +6,16 @@ import { Link } from 'react-router-dom';
 
 import Popup from 'reactjs-popup'; // popup import 
 import 'reactjs-popup/dist/index.css';
+import Modal from 'react-bootstrap/Modal';
+import Header from './Header';
+import PropTypes from 'prop-types';
+
+EnterInfo.PropTypes ={
+    type: PropTypes.string
+};
 export default function EnterInfo(props) {
 // console.log(options);
-const buttonType=props.type; //https://reactjs.org/docs/components-and-props.html
+const buttonType=props.type ; //https://reactjs.org/docs/components-and-props.html
 //TODO: conditional for if clicked on login or connect 
     // const [showResults, setShowResults]= React.userState(false);
     const inputBoxStyle={
@@ -39,12 +46,18 @@ const buttonType=props.type; //https://reactjs.org/docs/components-and-props.htm
         
     }
     
-const [hide]= useState(false);
+const [showModal, setShowModal]= useState(false);
+const closeModal = () => setShowModal(false);
+const openModal = () => setShowModal(true);
 
-    function submit(event){
-        event.preventDefault();
-        console.log("here");
-    };
+
+var hideSelf= false; // var for rendering a new headercomponent 
+
+   if(hideSelf){ 
+    console.log("Hi")
+       render(<Header/>);
+       
+   } //renders if we set hideself to true 
 
     return (
  
@@ -75,13 +88,33 @@ const [hide]= useState(false);
          id ="enter"
          className="btn"
          style={enterButtonStyle}
-         onClick= "onClick"
+         onClick= {hideSelf=true}
          ><Link to='/user' style={{color:'white'}}>{buttonType}
          </Link></Button>  {/*Link to userPage */}
         {/* Make a state to hide after logging in */}
 
         </form>
+
+        <Modal
+            show={showModal}
+            onHide={()=> setShowModal(false)}
+            dialogClassName="modal-90w"
+            arial-labelledby="example-custom-modal-styling-title"
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="modal_title">
+                    Connect to Spotify?
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>
+                    Are you sure that you want to connect to spotify?
+                </p>
+            </Modal.Body>
+        </Modal>
+    
      </div>
+
     
     );
     
